1411cef638c99902f6c42feb5b5d9c295c15e648
Urer260000 <128656584+Urer260000@users.noreply.github.com>
2023-09-17 14:39:13 -0400

    Update generateSecretKey.py

--- generateSecretKey.py
+++ generateSecretKey.py
@@ -1,8 +1,1 @@
 
-import secrets
-import base64
-
-def generate_secret_key():
-    return base64.b64encode(secrets.token_bytes(32)).decode('utf-8')
-
-print("Generated Secret Key:", generate_secret_key())
