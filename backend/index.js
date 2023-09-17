d289929bacba53d56b3f6f40d4b1dbb2ce5c085d
Urer260000 <128656584+Urer260000@users.noreply.github.com>
2023-09-17 14:12:43 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const rateLimit = require('express-rate-limit');
 const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const port = 3000;
 
-const jwtSecret = process.env.JWT_SECRET || 'fallbackSecret';
+const jwtSecret = process.env.JWT_SECRET || 'aMuchMoreComplexFallbackSecret';
 
 let client;
 
 MongoClient.connect("mongodb://localhost:27017/mydatabase", { useUnifiedTopology: true }, async function (err, mongoClient) {
     if (err) {
-        console.log("Error occurred while connecting to MongoDB Atlas...\n", err);
+        console.error("Error occurred while connecting to MongoDB Atlas...\n", err);
+        process.exit(1);
     } else {
         client = mongoClient;
         console.log('Connected...');
@@ -21,24 +24,40 @@ MongoClient.connect("mongodb://localhost:27017/mydatabase", { useUnifiedTopology
 
 app.use(express.json());
 
+// Apply rate limiting middleware
+const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100
+});
+app.use(apiLimiter);
+
 app.post('/register', async (req, res) => {
-    const newUser = {
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        createdAt: new Date(),
-        updatedAt: new Date()
-    };
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const newUser = {
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        };
 
-    await db.collection('users').insertOne(newUser);
+        await db.collection('users').insertOne(newUser);
 
-    const token = jwt.sign({ userId: newUser._id }, jwtSecret, { expiresIn: '1h' });
-    res.status(201).json({ message: 'User registered successfully', userId: newUser._id, token });
+        const token = jwt.sign({ userId: newUser._id }, jwtSecret, { expiresIn: '1h' });
+        res.status(201).json({ message: 'User registered successfully', userId: newUser._id, token });
+    } catch (err) {
+        res.status(500).json({ message: 'An error occurred while registering the user.' });
+    }
 });
 
 app.get('/services', async (req, res) => {
-    const services = await db.collection('services').find({}).toArray();
-    res.status(200).json({ services });
+    try {
+        const services = await db.collection('services').find({}).toArray();
+        res.status(200).json({ services });
+    } catch (err) {
+        res.status(500).json({ message: 'An error occurred while fetching services.' });
+    }
 });
 
 // Add middleware to verify JWT tokens
@@ -46,10 +65,6 @@ app.use(verifyToken);
 
 app.get('/protected', async (req, res) => {
     // Only authenticated users can access this route
-});
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
 });
 
 async function verifyToken(req, res, next) {
@@ -66,3 +81,7 @@ async function verifyToken(req, res, next) {
         return res.status(401).json({ message: 'Unauthorized' });
     }
 }
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
