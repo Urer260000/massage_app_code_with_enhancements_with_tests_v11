c1999dd1b2ba627a3a6a14612f1445226f4aeeb9
Urer260000 <128656584+Urer260000@users.noreply.github.com>
2023-09-19 14:39:31 +0000

    new

--- examples/platform_channel_swift/test_driver/button_tap_test.dart
+++ examples/platform_channel_swift/test_driver/button_tap_test.dart
@@ -1,6 +1,3 @@
-// Copyright 2014 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart' hide TypeMatcher, isInstanceOf;
